9256260c1698254c39e86bdb21f172d4d80d8bbc
reddappa <reddappareddyj@gmail.com>
2019-08-09 21:30:15 +0530

    DB utility

--- BDDframework/src/main/java/com/bdd/framework/utility/DBUtility.java
+++ BDDframework/src/main/java/com/bdd/framework/utility/DBUtility.java
@@ -37,20 +37,49 @@ public class DBUtility {
 	public static void MySqlconnection()
 	{
 		
-		try{  
+		try
+		{  
+			
+			// 1) Create a connection
+			
 			Class.forName("com.mysql.jdbc.Driver");  
+			
 			Connection con=DriverManager.getConnection(  
-			"jdbc:mysql://localhost:3306/TestDB","root","Password01");  
-			//here sonoo is database name, root is username and password  
-			Statement stmt=con.createStatement();  
-			System.out.println("Connected successfully.......");
-			ResultSet rs=stmt.executeQuery("select * from emp");  
-			while(rs.next())  
-				System.out.println("Connected successfully.......");
-			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-			con.close();  
-			}catch(Exception e){ System.out.println(e);}  
-			}  
+			"jdbc:mysql://localhost:3306/classicmodels","root","Password01"); 
+			
+			
+			// 2) Create statement/Query
+	          Statement stmt = con.createStatement();
+	          
+	          //3.Prepare Query
+	          String s = "select * from customerinfo  limit 1";
+	          
+	          
+	       // 3,4) Execute statement/Query & Store data in resultset
+	          ResultSet rs = stmt.executeQuery(s);
+	          
+	          
+	          while (rs.next()) {
+	              String bookname = rs.getString("BookName");
+	              String purchasedate = rs.getString("PurchasedDate");
+	              int amount = rs.getInt("Amount");
+	              String location = rs.getString("Location");
+	              System.out.println(bookname + "   " + purchasedate + "      " + amount+"    "+location);
+	          }
+
+	          // 5) close connection
+	          con.close();
+
+	          System.out.println("Query executed.....");
+	          
+	          
+		}
+		catch(Exception E) 
+		{
+			
+		}
+	}
+			
 
 
 public static void main(String args[])
--- BDDframework/src/main/java/com/bdd/framework/utility/XLUtils.java
+++ BDDframework/src/main/java/com/bdd/framework/utility/XLUtils.java
@@ -107,41 +107,4 @@ public class XLUtils {
 	return logindata;
 	}
 	
-	public static HashMap<String,String> storeValues = new HashMap<String, String>();
-	
-	  public static List<HashMap<String,String>> data(String filepath,String sheetName)
-	   {
-	      List<HashMap<String,String>> mydata = new ArrayList<>();
-	      try
-	      {
-	         FileInputStream fs = new FileInputStream(filepath);
-	         XSSFWorkbook workbook = new XSSFWorkbook(fs);
-	         XSSFSheet sheet = workbook.getSheet(sheetName);
-	         Row HeaderRow = sheet.getRow(0);
-	         for(int i=1;i<sheet.getPhysicalNumberOfRows();i++)
-	         {
-	            Row currentRow = sheet.getRow(i);
-	            HashMap<String,String> currentHash = new HashMap<String,String>();
-	            for(int j=0;j<currentRow.getPhysicalNumberOfCells();j++)
-	            {
-	               Cell currentCell = currentRow.getCell(j);
-	               switch (currentCell.getCellType())
-	               {
-	               case Cell.CELL_TYPE_STRING:
-	                  System.out.print(currentCell.getStringCellValue() + "\t");
-	                  currentHash.put(HeaderRow.getCell(j).getStringCellValue(), currentCell.getStringCellValue());
-	                  break;
-	               }
-	            }
-	            mydata.add(currentHash);
-	         }
-	         fs.close();
-	      }
-	      catch (Exception e)
-	      {
-	         e.printStackTrace();
-	      }
-	      return mydata;
-	   }
-	
 }
